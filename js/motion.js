22d4e1808fbc63dbb57a14f063691be5ff2b6695
malsllami <44532530+malsllami@users.noreply.github.com>
2026-01-23 01:29:07 +0300

    Delete js directory

--- js/motion.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.card').forEach(c=>{
-  c.addEventListener('mousemove',e=>{
-    c.style.transform='translateY(-6px)';
-  });
-});
--- js/pin.js
+++
@@ -1,7 +0,0 @@
-function copyPin(id){
-  const input = document.getElementById(id);
-  input.select();
-  input.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  alert("تم نسخ الرمز");
-}
